b9fb79c096b798bb1b068c6f23df27eb1d9d84bb
bchaindeveloper <bcdev3@protonmail.com>
2022-09-22 13:11:35 -0400

    VRFCord

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,12 @@
+const { developmentChains } = require("../helper-hardhat-config")
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deploymentsconst
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+
+    if (developmentChains.includes(netowrk.name)) {
+        log("local network detected! Deploying mocks...")
+        // deploy a mock vrf cordinator
+    }
 }
