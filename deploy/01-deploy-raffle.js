e9b3941f75651e65a3aa80beac66d6abe7299ecf
bchaindeveloper <bcdev3@protonmail.com>
2022-09-22 13:25:57 -0400

    entranceFee

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,9 +1,21 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    }
+
+    const networkFee = networkConfig[chainId]["entranceFee"]
+    const args = [vrfCoordinatorV2Address, entranceFee]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: [],
