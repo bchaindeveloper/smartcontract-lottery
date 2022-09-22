77a82c1fcac95580344ab0475e4ea44b9686391d
bchaindeveloper <bcdev3@protonmail.com>
2022-09-22 11:35:46 -0400

    env

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -4,7 +4,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: []
+        args: [],
         log: true,
     })
 }
