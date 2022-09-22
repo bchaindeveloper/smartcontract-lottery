4677abda53b5d2d3111e93cbb54a4860685971c9
bchaindeveloper <bcdev3@protonmail.com>
2022-09-22 18:13:52 -0400

    Staging

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,7 +5,7 @@ const networkConfig = {
     },
     31337: {
         name: "localhost",
-        subscriptionId: "588",
+        subscriptionId: "2194",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
@@ -13,7 +13,7 @@ const networkConfig = {
     },
     5: {
         name: "goerli",
-        subscriptionId: "6926",
+        subscriptionId: "2194",
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
