d66f2a2ddf84bac4c74e36d8ec0ea87e6e0b47df
bchaindeveloper <bcdev3@protonmail.com>
2022-09-21 20:02:39 -0400

    deploy-raffle

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,33 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY =
+const COINMARKETCAP_API_KEY =
+const ETHERSCAN_API_KEY =
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,   
+             },
+             goerli: {
+                chainId: 5,
+                blockConfirmations: 6,
+                url: 
+                accounts:
+             },
+    },
     solidity: "0.8.8",
+    nameAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
